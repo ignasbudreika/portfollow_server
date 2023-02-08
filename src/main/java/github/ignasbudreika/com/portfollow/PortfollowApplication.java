388565dd5d86d2dc62e03d96a6ffce7b23b850a1
Ignas <ign.budreika@gmail.com>
2023-02-08 23:40:50 +0200

    Initial commit

---
+++ src/main/java/github/ignasbudreika/com/portfollow/PortfollowApplication.java
@@ -0,0 +1,13 @@
+package github.ignasbudreika.com.portfollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfollowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfollowApplication.class, args);
+	}
+
+}
