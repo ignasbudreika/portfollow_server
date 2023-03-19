a895617541331035cfda2ee1e202eb6532bfe44a
ignasbudreika <ign.budreika@gmail.com>
2023-03-20 00:55:49 +0200

    Fix hardcoded SpectroCoin client values

--- src/main/java/com/github/ignasbudreika/portfollow/external/client/SpectroCoinClient.java
+++ src/main/java/com/github/ignasbudreika/portfollow/external/client/SpectroCoinClient.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -21,6 +22,19 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 @Component
 public class SpectroCoinClient {
+    private final String HTTP_REQUEST_HEADER_CONTENT_TYPE = "Content-Type";
+    private final String HTTP_REQUEST_HEADER_CONTENT_TYPE_JSON = "application/json";
+    private final String HTTP_REQUEST_HEADER_AUTHENTICATION = "Authentication";
+
+    @Value("${http.client.spectrocoin.oauth.url}")
+    private String oauthUrl;
+    @Value("${http.client.spectrocoin.oauth.scope}")
+    private String oauthScope;
+    @Value("${http.client.spectrocoin.oauth.version}")
+    private String oauthVersion;
+    @Value("${http.client.spectrocoin.wallet.url}")
+    private String walletUrl;
+
     @Autowired
     private HttpClient client;
 
@@ -35,23 +49,24 @@ public class SpectroCoinClient {
     public AccountsDTO getAccountData(String clientId, String clientSecret) throws IOException, URISyntaxException, InterruptedException {
         String accessToken = getAccessToken(clientId, clientSecret);
 
-        HttpResponse<String> response = client.send(HttpRequest.newBuilder(new URI("https://spectrocoin.com/api/r/wallet/accounts"))
-                .header("Authorization", String.format("Bearer %s", accessToken))
+        // todo extract this to env vars
+        HttpResponse<String> response = client.send(HttpRequest.newBuilder(new URI(walletUrl))
+                .header(HTTP_REQUEST_HEADER_AUTHENTICATION, String.format("Bearer %s", accessToken))
                 .GET().build(), HttpResponse.BodyHandlers.ofString());
 
         return wrappedObjectMapper.readValue(response.body(), AccountsDTO.class);
     }
 
     private String getAccessToken(String clientId, String clientSecret) throws IOException, URISyntaxException, InterruptedException {
-        URI uri = UriComponentsBuilder.fromUri(new URI("https://spectrocoin.com/api/r/oauth2/auth")).build().toUri();
+        URI uri = UriComponentsBuilder.fromUri(new URI(oauthUrl)).build().toUri();
 
         HttpResponse<String> response = client.send(HttpRequest.newBuilder(uri)
-                .header("Content-Type", "application/json")
+                .header(HTTP_REQUEST_HEADER_CONTENT_TYPE, HTTP_REQUEST_HEADER_CONTENT_TYPE_JSON)
                 .POST(
                     HttpRequest.BodyPublishers.ofString(
                         wrappedObjectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(AccessTokenRequestDTO.builder()
-                            .scope("user_account")
-                            .version("1.0")
+                            .scope(oauthScope)
+                            .version(oauthVersion)
                             .clientId(clientId)
                             .clientSecret(clientSecret).build()
                         ), StandardCharsets.UTF_8)
