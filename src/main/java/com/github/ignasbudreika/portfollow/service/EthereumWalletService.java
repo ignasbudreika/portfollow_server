b974354c5b186f4611d2c72cfbab98288805c2b5
ignasbudreika <ign.budreika@gmail.com>
2023-03-24 20:07:46 +0200

    EthereumWalletService bug fixes

--- src/main/java/com/github/ignasbudreika/portfollow/service/EthereumWalletService.java
+++ src/main/java/com/github/ignasbudreika/portfollow/service/EthereumWalletService.java
@@ -34,7 +34,7 @@ public class EthereumWalletService {
     public void addConnection(EthereumWalletConnectionDTO connectionDTO, User user) throws Exception {
         if (connectionRepository.findByAddress(connectionDTO.getAddress()) != null) {
             throw new EntityExistsException(String.format(
-                    "Ethereum wallet connection of address: {} for user: %s already exists", connectionDTO.getAddress(), user.getId()
+                    "Ethereum wallet connection of address: {} for user: {} already exists", connectionDTO.getAddress(), user.getId()
             ));
         }
 
@@ -75,7 +75,6 @@ public class EthereumWalletService {
         });
     }
 
-    @Transactional
     public void fetchBalance(EthereumWalletConnection connection, User user) {
         try {
             BigDecimal etherQuantity = walletHelper.getWalletBalanceInEther(connection.getAddress());
