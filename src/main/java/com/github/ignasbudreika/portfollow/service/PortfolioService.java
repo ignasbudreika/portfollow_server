43c817903cf264a184c2a521b5741839d890951b
ignasbudreika <ign.budreika@gmail.com>
2023-03-26 18:10:24 +0300

    Return all investments portfolio grouped by investment type

--- src/main/java/com/github/ignasbudreika/portfollow/service/PortfolioService.java
+++ src/main/java/com/github/ignasbudreika/portfollow/service/PortfolioService.java
@@ -14,6 +14,8 @@ import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class PortfolioService {
@@ -32,27 +34,39 @@ public class PortfolioService {
     public PortfolioDTO getUserPortfolio(User user) {
         Collection<InvestmentDTO> investments = investmentService.getUserInvestments(user);
 
-        return formatPortfolioFromInvestments(investments);
+        BigDecimal totalValue = investments.stream().map(InvestmentDTO::getValue).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        Map<InvestmentType, List<InvestmentDTO>> investmentsByType = investments.stream().collect(Collectors.groupingBy(InvestmentDTO::getType));
+
+        List<PortfolioDTO.DistributionDTO> distribution = investmentsByType.entrySet().stream().map(typeInvestments -> {
+            BigDecimal value = typeInvestments.getValue().stream().map(InvestmentDTO::getValue).reduce(BigDecimal.ZERO, BigDecimal::add);
+
+            return PortfolioDTO.DistributionDTO.builder()
+                    .label(typeInvestments.getKey().toString())
+                    .value(value)
+                    .percentage(value
+                            .divide(totalValue, 4, RoundingMode.HALF_UP)
+                            .multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP))
+                    .build();
+        }).toList();
+
+        return PortfolioDTO.builder().totalValue(totalValue).distribution(distribution).build();
     }
 
     public PortfolioDTO getUserPortfolioByType(User user, InvestmentType type) {
         Collection<InvestmentDTO> investments = investmentService.getUserInvestmentsByType(user, type);
 
-        return formatPortfolioFromInvestments(investments);
-    }
+        BigDecimal totalValue = investments.stream().map(InvestmentDTO::getValue).reduce(BigDecimal.ZERO, BigDecimal::add);
 
-    private PortfolioDTO formatPortfolioFromInvestments(Collection<InvestmentDTO> investments) {
-        BigDecimal totalValue = investments.stream().map(investment -> investment.getValue()).reduce(BigDecimal.ZERO, BigDecimal::add);
-
-        List<PortfolioDTO.DistributionDTO> distribution = investments.stream().map(investment -> {
-            return PortfolioDTO.DistributionDTO.builder()
+        List<PortfolioDTO.DistributionDTO> distribution = investments.stream().map(investment ->
+            PortfolioDTO.DistributionDTO.builder()
                     .label(investment.getSymbol())
                     .value(investment.getValue())
                     .percentage(investment.getValue()
                             .divide(totalValue, 4, RoundingMode.HALF_UP)
                             .multiply(BigDecimal.valueOf(100)).setScale(2, RoundingMode.HALF_UP))
-                    .build();
-        }).toList();
+                    .build()
+        ).toList();
 
         return PortfolioDTO.builder().totalValue(totalValue).distribution(distribution).build();
     }
