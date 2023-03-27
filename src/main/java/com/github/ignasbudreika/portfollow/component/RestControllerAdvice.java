fb14f766bc0c68ca154c04607e6da15c0a381f25
ignasbudreika <ign.budreika@gmail.com>
2023-03-28 01:50:08 +0300

    Improve exception logging

--- src/main/java/com/github/ignasbudreika/portfollow/component/RestControllerAdvice.java
+++ src/main/java/com/github/ignasbudreika/portfollow/component/RestControllerAdvice.java
@@ -15,25 +15,25 @@ import jakarta.persistence.EntityExistsException;
 public class RestControllerAdvice {
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ApiErrorDTO> handleUnauthorized(UnauthorizedException exception) {
-        log.info("handling unauthorized: {}", exception.getMessage());
+        log.info("handling unauthorized", exception);
         return ResponseEntity.status(401).body(ApiErrorDTO.builder().key("unauthorized").message("access to this resource requires authorization").build());
     }
 
     @ExceptionHandler({EntityExistsException.class})
     public ResponseEntity<ApiErrorDTO> handleEntityExists(EntityExistsException exception) {
-        log.info("handling entity exists: {}", exception.getMessage());
+        log.info("handling entity exists", exception);
         return ResponseEntity.badRequest().body(ApiErrorDTO.builder().key("entity_exists").message("entity already exists").build());
     }
 
     @ExceptionHandler({BusinessLogicException.class})
     public ResponseEntity<ApiErrorDTO> handleBusinessLogic(BusinessLogicException exception) {
-        log.info("handling business logic: {}", exception.getMessage());
+        log.warn("handling business logic", exception);
         return ResponseEntity.badRequest().body(ApiErrorDTO.builder().key("business_logic").message("invalid request").build());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorDTO> handleException(Exception exception) {
-        log.error("exception occurred: {}", exception.getMessage());
+        log.error("exception occurred", exception);
         return ResponseEntity.internalServerError().body(ApiErrorDTO.builder().key("error occurred").message("something went wrong").build());
     }
 }
